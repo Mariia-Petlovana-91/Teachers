3f568d85bcd758512eef612e13333d4ac7756428
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-04-04 23:18:06 +0300

    add style about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,26 @@
-import { FaPlus } from 'react-icons/fa6';
-
 const About = () => {
   return (
     <section className="about__section">
       <ul className="about__list">
         <li className="about__item">
           <span className="about__number">32,000</span>
-          <FaPlus className="about__icon" />
-          <span className="about__text">Experienced tutors</span>
+          <span className="about__number"> + </span>
+          <p className="about__text">Experienced tutors</p>
         </li>
         <li className="about__item">
           <span className="about__number">300,000</span>
-          <FaPlus className="about__icon" />
-          <span className="about__text">5-star tutor reviews</span>
+          <span className="about__number"> + </span>
+          <p className="about__text">5-star tutor reviews</p>
         </li>
         <li className="about__item">
           <span className="about__number">120</span>
-          <FaPlus className="about__icon" />
-          <span className="about__text">Subjects taught</span>
+          <span className="about__number"> + </span>
+          <p className="about__text">Subjects taught</p>
         </li>
         <li className="about__item">
           <span className="about__number">200</span>
-          <FaPlus className="about__icon" />
-          <span className="about__text">Tutor nationalities</span>
+          <span className="about__number"> + </span>
+          <p className="about__text"> Tutor nationalities</p>
         </li>
       </ul>
     </section>
