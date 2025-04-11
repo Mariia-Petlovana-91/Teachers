f887e921619848243332e87e419f6380bce503dc
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-04-12 02:39:34 +0300

    add not found page and style for not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,11 @@
-const NotFound = () => {};
+import notFoundImg from '../img/notFound.jpg';
+
+const NotFound = () => {
+  return (
+    <div className="page">
+      <img className="page__not-found-imd" src={notFoundImg} alt="not found image" />
+    </div>
+  );
+};
 
 export default NotFound;
