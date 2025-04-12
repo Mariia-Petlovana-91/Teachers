bbcd8482a9a0813aeaf9320f706af92d9e2d34fb
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-04-12 03:33:42 +0300

    fix favorite array

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,16 +1,16 @@
 import TeacherList from '../components/TeacherList/TeacherList';
 
 const Favorites = () => {
-  const favorites = JSON.parse(localStorage.getItem('favorites') || []);
+  const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
 
   return (
     <div className="page__teachers">
-      {favorites.length > 0 ? (
-        <TeacherList array={favorites} />
-      ) : (
+      {favorites.length === 0 ? (
         <div className="page__favorite-not-item">
           🤷‍♀️ You haven't added any favorite teachers yet.
         </div>
+      ) : (
+        <TeacherList array={favorites} />
       )}
     </div>
   );
